4cc9ec178acf5bfd11d439f6d1324fce1c2b1050
Linuxe <angelren1220@gmail.com>
2023-04-18 16:31:00 -0400

    add DayListItem styles

--- stories/index.js
+++ stories/index.js
@@ -29,8 +29,8 @@ storiesOf("Button", module)
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
   })
   .add("Unselected", () => <DayListItem name="Monday" spots={5} />)
-  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />)
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
   .add("Full", () => <DayListItem name="Monday" spots={0} />)
   .add("Clickable", () => (
-    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5}/>
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
   ));
