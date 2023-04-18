e4ab7c8bc41e8b33a8c391377d7ab92f99a28aa0
Linuxe <angelren1220@gmail.com>
2023-04-18 16:41:53 -0400

    add DayListItem test and refactor format

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,15 +4,26 @@ import "components/DayListItem.scss";
 import classNames from "classnames";
 
 export default function DayListItem(props) {
+  const formatSpots = () => {
+    if (!props.spots) {
+      return `no spots remaining`;
+    }
+    if (props.spots === 1) {
+      return `${props.spots} spot remaining`;
+    }
+    return `${props.spots} spots remaining`;
+  };
+
   const dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
     'day-list__item--full': !props.spots
   });
 
+  const spotsInfo = formatSpots();
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{spotsInfo}</h3>
     </li>
   );
 }
