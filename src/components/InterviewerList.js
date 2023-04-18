022ff6c8fcfb1cb8d3154656377f08194b0571d2
Linuxe <angelren1220@gmail.com>
2023-04-18 19:11:08 -0400

    add InterviewerListItem, stories, scss

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function InterviewerList(props) {
+
+  return (
+    <ul>
+      <InterviewerListItem />
+    </ul>
+  );
+}
