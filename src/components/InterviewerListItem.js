fe3bf68eb500c5859ac014def8cebadc9c0330ee
Linuxe <angelren1220@gmail.com>
2023-04-19 14:15:40 -0400

    refactor interviewer components

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -10,17 +10,17 @@ export default function InterviewerListItem(props) {
     'interviewers__item--selected': props.selected
   });
 
-  const nameDisplay = props.selected ? props.name : "";
-
   return (
-    <li className={interviewerClass}>
+    <li
+      className={interviewerClass}
+      onClick={props.setInterviewer}
+    >
       <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
-        onClick={() => props.setInterviewer(props.id)}
       />
-      {nameDisplay}
+      {props.selected && props.name}
     </li>
   );
 }
