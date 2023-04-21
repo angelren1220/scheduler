c6d8b5c0df19372751f8e54bccfb342660d6410f
Linuxe <angelren1220@gmail.com>
2023-04-21 13:50:11 -0400

    retrive interviewer data

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,7 +5,7 @@ import "components/Application.scss";
 
 import DayList from "components/DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
 
@@ -21,19 +21,24 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       axios.get("/api/days"),
-      axios.get("/api/appointments")
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then((all) => {
-      const [days, appointments] = all;
-      setState(prev => ({...prev, days: days.data, appointments: appointments.data }));
+      const [days, appointments, interviewers] = all;
+      setState(prev => ({ ...prev, days: days.data, appointments: appointments.data, interviewers: interviewers.data }));
     });
   }, []);
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const appointmentsList = Object.values(dailyAppointments).map((appointment) => {
+
+    const interview = getInterview(state, appointment.interview);
     return (<Appointment
       key={appointment.id}
-      {...appointment}
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview}
     />);
   });
 
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,9 +1,6 @@
-
 export function getAppointmentsForDay(state, day) {
   //... returns an array of appointments for that day
-  if (!day) {
-    return [];
-  }
+  if (!day) return [];
 
   const findDay = state.days.find(
     data => data.name === day);
@@ -15,4 +12,17 @@ export function getAppointmentsForDay(state, day) {
   });
 
   return appointments;
+}
+
+export function getInterview(state, interview) {
+  if (!interview) return null;
+
+  const scheduledInterview = {
+    interviewer: state.interviewers[interview.interviewer],
+    student: interview.student
+  };
+
+  return scheduledInterview;
+
+
 }
